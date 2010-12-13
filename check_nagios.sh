616d712253c2fb65ace8b884ae85c67812f05083
Elan Ruusamäe <glen@pld-linux.org>
2010-12-13 18:40:30 +0000

    - debug off by default
    Changed files:
        check_nagios.sh -> 1.2

--- check_nagios.sh
+++ check_nagios.sh
@@ -1,9 +1,8 @@
 #!/bin/sh
 # vim:ft=sh
 # enable for debugging:
-set -x
-exec 2>>/tmp/nagios-cacti.log
-#exec 1>&2
+#set -x
+#exec 2>>/tmp/nagios-cacti.log
 
 url="$1"
 type="$2"
@@ -17,4 +16,3 @@ url="${url%/}"
 # this way we can even have passwords in them.
 # retry only once with timeout of 30s
 wget -t 1 -T 30 --no-check-certificate -q -O- ${user:+--user="$user" --password="$password"} "$url/mrtgstats.cgi?type=$type"
-echo $? >&2
